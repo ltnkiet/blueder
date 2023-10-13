0fa9acec571b54833e7b682092597f54f2993953
Lưu Khoa Học <lkhoc200443@gmail.com>
2023-10-13 19:10:29 +0700

    Modify scan screen (#3)
    * rename home to prehome, add some screen for home
    
    * complete FE for scan screen
    
    ---------
    
    Co-authored-by: ltnkiet <89096536+ltnkiet@users.noreply.github.com>

--- screens/Scan.jsx
+++ screens/Scan.jsx
@@ -1,14 +1,18 @@
+
 import React from 'react'
 import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
 
+
 // Import icon
 import { Feather } from '@expo/vector-icons'
+
 
 
 export default function Scan() {
   return (
 <View className='w-full h-full flex items-center'>
       <View className='flex-row items-center justify-between w-11/12 py-6'>
+
         <Text className='flex flex-row items-center font-bold text-4xl'>
           <Text className='text-blue-700'>Blue</Text>
           <Text>der</Text>
@@ -17,6 +21,7 @@ export default function Scan() {
           <Feather name='bell' size={30} color='#000AFFE0' />
         </TouchableOpacity>
       </View>
+
       <View className='flex-row items-center justify-between border rounded-3xl w-11/12 mb-6'>
         <View className='px-3'>
           <Feather name='search' size={24} color='black' />
@@ -41,6 +46,7 @@ export default function Scan() {
           suscipit iste nemo quo dignissimos nam fuga consequatur repellat aliquam
           id omnis, vel earum nobis. Impedit modi quos ratione. Officiis.
         </Text>
+
       </View>
     </View>
   )
